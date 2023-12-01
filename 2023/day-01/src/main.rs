f6725e05efc9e72536d651e9cee7c9250dd38d0a
git-avilabs <mail.avinashsah@gmail.com>
2023-12-02 00:25:49 +0530

    part 2 solution

--- 2023/day-01/src/main.rs
+++ 2023/day-01/src/main.rs
@@ -9,11 +9,49 @@ fn get_digits(puzzle_line: &str) -> Vec<&str> {
     results
 }
 
+fn convert_to_digits(puzzle_line: &str) -> String {
+    let parsed_string = puzzle_line
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e");
+
+    parsed_string
+}
+
 fn solve_part_1(puzzle_lines: Split<'_, char>) -> u32 {
     let mut total_sum = 0;
 
     for line in puzzle_lines {
         let digits = get_digits(line);
+        let digits_length = digits.len();
+
+        if digits_length == 0 {
+            continue;
+        }
+
+        let first_digit = digits[0];
+        let last_digit = digits[digits_length - 1];
+        let joinned_digit_str = format!("{first_digit}{last_digit}");
+        let joinned_digit = joinned_digit_str.parse::<u32>().unwrap();
+
+        total_sum += joinned_digit;
+    }
+
+    total_sum
+}
+
+fn solve_part_2(puzzle_lines: Split<'_, char>) -> u32 {
+    let mut total_sum = 0;
+
+    for line in puzzle_lines {
+        let patched_line = convert_to_digits(line);
+        let digits = get_digits(patched_line.as_str());
         let digits_length = digits.len();
 
         if digits_length == 0 {
@@ -39,8 +77,10 @@ fn main() {
         let puzzle_input = fs::read_to_string(puzzle_input_path).unwrap();
         let puzzle_lines = puzzle_input.split('\n');
 
-        let total_sum = solve_part_1(puzzle_lines.clone());
+        let solution_part_1 = solve_part_1(puzzle_lines.clone());
+        let solution_part_2 = solve_part_2(puzzle_lines);
 
-        println!("Part-1 Solution: {total_sum}")
+        println!("Part-1 Solution: {solution_part_1}");
+        println!("Part-2 Solution: {solution_part_2}");
     }
 }
